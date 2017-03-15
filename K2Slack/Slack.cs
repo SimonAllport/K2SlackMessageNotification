8e070bff38515a7cefc440f9dfe4c6e5c302c88a
sallport <salllport@channel4.co.uk>
2017-03-15 10:51:00 +0000

    Base

---
+++ K2Slack/Slack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace K2Slack
+{
+    public class SlackClient
+    {
+        private readonly Uri _uri;
+     
+
+        //Post a message using simple strings
+        public static void PostMessage(string text, string username = null, string channel = null)
+        {
+            Payload payload = new Payload()
+            {
+                Channel = channel,
+                Username = username,
+                Text = text
+            };
+            
+            PostMessage(payload);
+        }
+
+        //Post a message using a Payload object
+        private static void PostMessage(Payload payload)
+        {
+              Encoding _encoding = new UTF8Encoding();
+            Uri endpoint = new Uri("webhook url here");
+
+        string payloadJson = JsonConvert.SerializeObject(payload);
+            
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                System.Collections.Specialized.NameValueCollection data = new System.Collections.Specialized.NameValueCollection();
+                data["payload"] = payloadJson;
+
+                var response = client.UploadValues(endpoint, "POST", data);
+
+                //The response text is usually "ok"
+                string responseText = _encoding.GetString(response);
+            }
+        }
+    }
+
+    //This class serializes into the Json payload required by Slack Incoming WebHooks
+    public class Payload
+    {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ K2SlackTestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2SlackTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            K2Slack.SlackClient.PostMessage(" @Stevefisher Hello World","stevefisher","general") ;
+        }
+    }
+}
